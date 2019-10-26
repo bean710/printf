c0929f233e4276839c8520f9f1cacdf97f60161f
Beardocracy <travisjbearden@gmail.com>
2019-10-26 21:15:33 +0000

    Added documentation for string print

--- string_print.c
+++ string_print.c
@@ -1,12 +1,17 @@
 #include "holberton.h"
 
+/**
+ * print_string - prints a string
+ * @sp: void pointer to a string
+ * Return: The number of characters printed.
+ */
 int print_string(void *sp)
 {
 	char *s = *(char **)sp;
 	int i = 0;
 
 	for (; *s; s++, i++)
-		putchar(*s);
+		_putchar(*s);
 
 	return (i);
 }
