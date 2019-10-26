510f414c3838540fae651f00d9e18297c2e87574
Ben Keener <keener4christ@gmail.com>
2019-10-26 19:13:02 +0000

    Refactored filebase

---
+++ decimal_print.c
@@ -0,0 +1,89 @@
+#include "holberton.h"
+
+int print_decimal(void *ip)
+{
+	int i = *(int *)ip;
+
+	print_number(i);
+
+	return (getlen(i));
+}
+
+/**
+ * print_number - prints a number
+ * @n: number to print
+ */
+void print_number(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+		putchar('-');
+		for (i = 0; i < getlen(n); i++)
+			putchar('0' + -getnum(n, i));
+	}
+	else
+	{
+		for (i = 0; i < getlen(n); i++)
+			putchar('0' + getnum(n, i));
+	}
+}
+
+/**
+ * _pow - Calculates exponents
+ * @a: base
+ * @b: exponent
+ *
+ * Return: a^b
+ */
+int _pow(int a, int b)
+{
+	int i, ans;
+
+	if (b <= 0)
+		return (1);
+
+	ans = a;
+	for (i = 0; i < b - 1; i++)
+	{
+		ans *= a;
+	}
+
+	return (ans);
+}
+
+/**
+ * getnum - gets the 'index'th digit of num, starting from the left at 0
+ * @num: the number to extract digit from
+ * @index: the index of the desired digit with 0 being the leftmost digit
+ *
+ * Return: The 'index'th digit of num
+ */
+int getnum(int num, int index)
+{
+	int l;
+
+	l = getlen(num);
+	num = num / _pow(10, l - 1 - index);
+	return (num % 10);
+}
+
+/**
+ * getlen - gets the length of the number num
+ * @num: the number to get length of
+ *
+ * Return: length of number 'num'
+ */
+int getlen(int num)
+{
+	int i;
+
+	i = 1;
+	while (num > 9 || num < -9)
+	{
+		i++;
+		num /= 10;
+	}
+	return (i);
+}
