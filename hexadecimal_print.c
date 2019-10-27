5085b74bbe9aa9a6d26be2e2df5c80da4fd7aea1
Ben Keener <keener4christ@gmail.com>
2019-10-27 03:29:25 +0000

    Changed hex printing function to take a set

--- hexadecimal_print.c
+++ hexadecimal_print.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
 #include <stdio.h>
 
-int dec_to_hex(unsigned int i)
+int dec_to_hex(unsigned int i, char *set)
 {
 	int count = 1;
-	char digs[] = "0123456789abcdef";
 	if (i > 15)
-		count += dec_to_hex(i / 16);
+		count += dec_to_hex(i / 16, set);
 
-	_putchar(digs[i % 16]);
+	_putchar(set[i % 16]);
 
 	return (count);
 }
@@ -17,6 +16,7 @@ int print_hex(void *uhp)
 {
 	unsigned int i = 1, count = 0;
 	unsigned int hex = *(unsigned int *)uhp;
+	char *set = "0123456789abcdef";
 
-	return(dec_to_hex(hex));
+	return(dec_to_hex(hex, set));
 }
