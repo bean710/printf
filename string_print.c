898f8ead101d24a06986fcb8f9c821a3dd7c9410
Beardocracy <travisjbearden@gmail.com>
2019-10-27 23:39:57 +0000

    Added if statement for NULL

--- string_print.c
+++ string_print.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stddef.h>
 /**
  * print_string - prints a string
  * @sp: void pointer to a string
@@ -10,6 +10,16 @@ int print_string(void *sp)
 	char *s = *(char **)sp;
 	int i = 0;
 
+	if (s == NULL)
+	{
+		_putchar('(');
+		_putchar('n');
+		_putchar('u');
+		_putchar('l');
+		_putchar('l');
+		_putchar(')');
+		return (6);
+	}
 	for (; *s; s++, i++)
 		_putchar(*s);
 
