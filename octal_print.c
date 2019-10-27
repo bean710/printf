597cf7308d213884f1c2df1f3862463bec605b69
Ben Keener <keener4christ@gmail.com>
2019-10-27 19:23:31 +0000

    Added documentation

--- octal_print.c
+++ octal_print.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+ * print_oct - Prints an unsigned int as an octal
+ * @uop: Void pointer to an adress in memory holding an unsigned int
+ *
+ * Return: Number of characters printed
+ */
 int print_oct(void *uop)
 {
 	unsigned int oct = *(unsigned int *)uop;
