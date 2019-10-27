bb45d855e1656ea2cc618dbafbf165cfcb50832c
Beardocracy <travisjbearden@gmail.com>
2019-10-27 19:43:25 +0000

    Added binary function for testing

--- binary.c
+++ binary.c
@@ -1,3 +1,39 @@
 #include "holberton.h"
 
-Write code here
+/**
+ * print_binary - Prints a number in binary.
+ * @ip: A pointer to an unsigned int
+ * Return: the number of characters printed.
+ */
+int print_binary(void *ip)
+{
+	unsigned int n = *(unsigned int *)ip;
+	int i;
+	int count = 0;
+
+	for (i = 31; i >= 0; i--)
+	{
+		if (n / _pow(2, i) > 0 && count == 0)
+		{
+			_putchar('1');
+			count++;
+			n -= _pow(2, i);
+		}
+		if (count > 0)
+		{
+
+			if (n / _pow(2, i) > 0)
+			{
+				_putchar('1');
+				n -= _pow(2, i);
+				count++;
+			}
+			else if (n / _pow(2, i) == 0)
+			{
+				_putchar('0');
+				count++;
+			}
+		}
+	}
+	return (count);
+}
