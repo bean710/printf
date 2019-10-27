4d12a199b4a125d8c5cc41ecaffce99c624d76b6
Ben Keener <keener4christ@gmail.com>
2019-10-27 03:00:02 +0000

    Made working dec to hex converter using recursion

--- hexadecimal_print.c
+++ hexadecimal_print.c
@@ -1,24 +1,22 @@
 #include "holberton.h"
+#include <stdio.h>
 
-int print_hex(void *uph)
+int dec_to_hex(unsigned int i)
+{
+	int count = 1;
+	char digs[] = "0123456789abcdef";
+	if (i > 15)
+		count += dec_to_hex(i / 16);
+
+	_putchar(digs[i % 16]);
+
+	return (count);
+}
+
+int print_hex(void *uhp)
 {
 	unsigned int i = 1, count = 0;
 	unsigned int hex = *(unsigned int *)uhp;
-	char hex_digs[] = "0123456789abcdef";
 
-	if (hex == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-
-	while (hex / (i * 16))
-		i *= 16;
-
-	while (i > 1)
-	{
-		_putchar(hex_digs[(hex % i)]);
-		i /= 16;
-	}
+	return(dec_to_hex(hex));
 }
-
