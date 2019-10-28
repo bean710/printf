8122e9c8ec260654a3254ac46ffd0c7438d9749b
Beardocracy <travisjbearden@gmail.com>
2019-10-28 01:44:25 +0000

    Adding new function before testing

---
+++ print_reverse.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_reverse - prints a string in reverse.
+ * @s: a void pointer to a string
+ * Return: the number of characters
+ */
+int print_reverse(void *s)
+{
+	char *arr = *(char **)s;
+	int i = 0;
+	int count = 0;
+
+	for (i = 0; arr[i] != '\0'; i++)
+		;
+	i--;
+	for ( ; i > - 1; i--)
+	{
+		_putchar(arr[i]);
+		count++;
+	}
+	return (count);
+}
