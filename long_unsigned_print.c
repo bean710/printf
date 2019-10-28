a4d2f54538a9228a21ae685ee23f33fab03b4110
Beardocracy <travisjbearden@gmail.com>
2019-10-28 20:22:10 +0000

    Created function to print long unsigned ints in base 10

---
+++ long_unsigned_print.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_unsig_long - Prints an unsigned long integer
+ * @uip: Void opinter to an address in memory which holds an unsigned long
+ * integer.
+ * Return: The number of characters printed.
+ */
+int print_unsig_long(void *luip)
+{
+	unsigned long int i = *(unsigned int *)luip;
+
+	return (long_base_converter(i, 10, "0123456789");
+}
