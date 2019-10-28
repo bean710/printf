78457f3e7206b88f5c5a3b5dd7e41ae3672a9817
Beardocracy <travisjbearden@gmail.com>
2019-10-28 20:55:35 +0000

    Created func to print unigned short ints

---
+++ short_unsigned_print.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_unsig_short - Prints an unsigned short integer in base 10.
+ * @suip: Void opinter to an address in memory which holds an unsigned short
+ * integer.
+ * Return: The number of characters printed.
+ */
+int print_unsig_short(void *suip)
+{
+	unsigned short int i = *(unsigned short int *)luip;
+
+	return (short_base_conv(i, 10, "0123456789");
+}
