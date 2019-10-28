69d8fa6014cc74e2664c11c4d3b1db3d55b3a129
Beardocracy <travisjbearden@gmail.com>
2019-10-28 01:55:56 +0000

    Added for case of NULL string

--- print_reverse.c
+++ print_reverse.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stddef.h>
 
 /**
  * print_reverse - prints a string in reverse.
@@ -11,6 +12,16 @@ int print_reverse(void *s)
 	int i = 0;
 	int count = 0;
 
+	if (arr == NULL)
+	{
+		_putchar('(');
+		_putchar('n');
+		_putchar('u');
+		_putchar('l');
+		_putchar('l');
+		_putchar(')');
+		return (6);
+	}
 	for (i = 0; arr[i] != '\0'; i++)
 		;
 	i--;
