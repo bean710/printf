c08091bbeaca461593dce42d276b773724db6db5
Ben Keener <keener4christ@gmail.com>
2019-10-29 23:52:45 +0000

    Fixed issue by checking against null byte

--- flags.c
+++ flags.c
@@ -88,7 +88,7 @@ int setspecifier(const char *string, param *p, spec *specs)
 	int offset = 0;
 	int i;
 
-	for (i = 0; (specs + i)->spec_string != NULL; i++)
+	for (i = 0; (specs + i)->type != '\0'; i++)
 	{
 		for (offset = 0; string[offset] &&
 				string[offset] == (specs + i)->spec_string[offset]; offset++)
@@ -123,32 +123,32 @@ int get_full(const char *string, spec *specs, param *res)
 	int offset = 0;
 	int specifier;
 
-	/*
-	 * printf("Getting params for: %s\n", string);
-	 */
+
+	/* printf("Getting params for: %s\n", string); */
+
 
 	offset = setflags(string, res);
-	/*
-	 * printf("Got flags: %d, %d, %d, %d\n", res->plus, res->minus,
-	 * res->zero, res->space);
-	 */
+
+	/* printf("Got flags: %d, %d, %d, %d\n", res->plus, res->minus, */
+	/* res->zero, res->space); */
+
 	offset += setwidth(string + offset, res);
-	/*
-	 * printf("Got width: %d\n", res->width);
-	 */
+
+	/* printf("Got width: %d\n", res->width); */
+	
 	offset += setprecision(string + offset, res);
-	/*
-	 * printf("Got precision: %d\n", res->precision);
-	 */
+
+	/* printf("Got precision: %d\n", res->precision); */
+
 
 	specifier = setspecifier(string + offset, res, specs);
 	if (specifier == -1)
 		return (-1);
 
 	offset += specifier;
-	/*
-	 * printf("Got specifier: %s\n", res->specifier->spec_string);
-	 */
+
+	/* printf("Got specifier: %s\n", res->specifier->spec_string); */
+
 
 	return (offset);
 }
